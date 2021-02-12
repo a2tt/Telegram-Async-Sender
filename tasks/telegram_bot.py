f0f7e5c8bef35e615be21dda9df489b9afbc255a
usera2tt <usera2tt@gmail.com>
2021-02-12 22:41:16 +0900

    reply_markup 처리

--- tasks/telegram_bot.py
+++ tasks/telegram_bot.py
@@ -1,8 +1,10 @@
 import typing
 import time
 import math
+from collections.abc import Iterable
 
 import telegram
+from telegram import ReplyKeyboardMarkup
 from telegram.error import RetryAfter, TimedOut, NetworkError
 
 from configs import RETRY
@@ -15,19 +17,23 @@ def send_message(token: str, chat_id: typing.Union[int, str], text: str,
     :param chat_id: chat_id | channel_id | ...
     :param text:
     :param parse_mode: None | 'HTML'
-    :param reply_markup:
+    :param reply_markup: list containing list. A list forms a row.
     """
     bot = telegram.Bot(token)
 
     if not text:
         return None
 
+    if isinstance(reply_markup, Iterable):
+        reply_markup = ReplyKeyboardMarkup(reply_markup, one_time_keyboard=True)
+
     splitted = split_message(text, parse_mode)
     for msg in splitted:
         for idx in range(0, RETRY):
             try:
                 bot.send_message(chat_id=chat_id, text=msg, timeout=5,
-                                 parse_mode=parse_mode, reply_markup=reply_markup,
+                                 parse_mode=parse_mode,
+                                 reply_markup=reply_markup,
                                  disable_web_page_preview=True)
                 break
             except TimedOut:  # 요청이 갔을 가능성이 있으므로
